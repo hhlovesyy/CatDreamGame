ffa6bd6f304732a4ecaba8fb5b65c1944c9b3145
hhlovesyy <1596944152@qq.com>
2024-11-28 00:35:18 +0800

    完成了开始界面的视觉效果

--- Assets/Programmer/Framework/Application/UIViews/GameWelcomePanel.cs
+++ Assets/Programmer/Framework/Application/UIViews/GameWelcomePanel.cs
@@ -1,27 +1,32 @@
 using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
 
 namespace OurGameFramework
 {
     public class GameWelcomePanel : UIView
     {
         #region 控件绑定变量声明，自动生成请勿手改
-		#pragma warning disable 0649
-		[ControlBinding]
-		private Button StartGameBotton;
-		[ControlBinding]
-		private Button SettingButton;
-		[ControlBinding]
-		private Button ExitButton;
+#pragma warning disable 0649
+        [ControlBinding]
+        public Button StartGameBotton;
+        [ControlBinding]
+        public RectTransform TweenYinfu;
+        [ControlBinding]
+        public Button SettingButton;
+        [ControlBinding]
+        public Button ExitButton;
 
-		#pragma warning restore 0649
+#pragma warning restore 0649
         #endregion
-
-
+        
+        private List<Tween> yinfuTweens = new List<Tween>();
+        
         private void StartGame()
         {
             Debug.Log("Start Game!");
@@ -55,9 +60,87 @@ namespace OurGameFramework
             ExitButton.onClick.AddListener(ExitGame);
         }
 
+        private void DoYinfuTween()
+        {
+            // 音符有移动，有缩放，有透明度变化,具备一定的随机性
+            for (int i = 0; i < TweenYinfu.childCount; i++)
+            {
+                RectTransform yinfu = TweenYinfu.GetChild(i).GetComponent<RectTransform>();
+
+                // 随机初始化位置、缩放和透明度
+                Vector3 targetPosition = new Vector3(UnityEngine.Random.Range(-300, 300), UnityEngine.Random.Range(-300, 300), 0);
+                float moveDuration = Random.Range(3f, 5f);
+
+                // 初始化移动的Tween
+                var moveTween = yinfu.DOLocalMove(targetPosition, moveDuration)
+                    .SetLoops(-1, LoopType.Yoyo)
+                    .SetEase(Ease.Linear);
+
+                // 使用局部变量保存moveTween，用于在OnStepComplete中访问
+                moveTween.OnStepComplete(() =>
+                {
+                    Vector3 currentPos = yinfu.localPosition;
+                    targetPosition = new Vector3(UnityEngine.Random.Range(-300, 300), UnityEngine.Random.Range(-300, 300), 0);
+                    //等待两秒
+                    moveTween.ChangeStartValue(currentPos); // 更新起始位置
+                    moveTween.ChangeEndValue(targetPosition); // 更新目标位置
+                });
+
+                // 随机初始化缩放
+                Vector3 targetScale = Vector3.one * UnityEngine.Random.Range(0.5f, 1.5f);
+                float scaleDuration = Random.Range(1f, 2f);
+
+                // 初始化缩放的Tween
+                var scaleTween = yinfu.DOScale(targetScale, scaleDuration)
+                    .SetLoops(-1, LoopType.Yoyo)
+                    .SetEase(Ease.Linear);
+
+                // 使用局部变量保存scaleTween，用于在OnStepComplete中访问
+                scaleTween.OnStepComplete(() =>
+                {
+                    Vector3 currentScale = yinfu.localScale;
+                    targetScale = Vector3.one * UnityEngine.Random.Range(0.5f, 1.5f);
+                    //scaleTween.ChangeEndValue(targetScale); // 更新目标缩放
+                    scaleTween.ChangeStartValue(currentScale); // 更新起始缩放
+                    scaleTween.ChangeEndValue(targetScale); // 更新目标缩放
+                });
+
+                // 随机初始化透明度
+                float targetAlpha = 1f;
+                float fadeDuration = Random.Range(1f, 2f);
+
+                // 初始化透明度的Tween
+                var fadeTween = yinfu.GetComponent<CanvasGroup>().DOFade(targetAlpha, fadeDuration)
+                    .SetLoops(-1, LoopType.Yoyo)
+                    .SetEase(Ease.Linear);
+
+                // 使用局部变量保存fadeTween，用于在OnStepComplete中访问
+                fadeTween.OnStepComplete(() =>
+                {
+                    float thisAlpha = yinfu.GetComponent<CanvasGroup>().alpha;
+                    targetAlpha = thisAlpha > 0.9f ? 0f : 1f;
+                    fadeTween.ChangeStartValue(thisAlpha); // 更新起始透明度
+                    fadeTween.ChangeEndValue(targetAlpha); // 更新目标透明度
+                    //fadeTween.ChangeEndValue(targetAlpha); // 更新目标透明度
+                });
+
+                // 添加到yinfuTweens列表（如果需要的话）
+                yinfuTweens.Add(moveTween);
+                yinfuTweens.Add(scaleTween);
+                yinfuTweens.Add(fadeTween);
+            }
+        }
+
+
         public override void OnOpen(object userData)
         {
             base.OnOpen(userData);
+            for(int i=0;i<yinfuTweens.Count;i++)
+            {
+                yinfuTweens[i].Kill();
+            }
+            yinfuTweens.Clear();
+            DoYinfuTween();
             HAudioManager.Instance.Play("WelcomePanelMusic",HGameRoot.Instance.gameObject);
         }
 
